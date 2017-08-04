733de26b45c7f0042850ac88341c6a0dd4636f22
Nikki Wong <NikkiWong@ecf.ie>
2017-08-04 09:48:33 +0100

    bug fixed, forgot to add in the input for PlaylistComponent

--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -2,8 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'playlist',
-    templateUrl: 'app/ts/playlist.component.html'
-    inputs: 
+    templateUrl: 'app/ts/playlist.component.html',
+    inputs: ['videos']
 })
 
 export class PlaylistComponent{}
