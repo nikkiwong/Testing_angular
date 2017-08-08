19cc31cd9e42e6d0564c6291a91e6cd3747c8170
Nikki Wong <NikkiWong@ecf.ie>
2017-08-08 14:09:45 +0100

    implemented a toggle function for when user clicks the row, the video will display and then when clicked again it will hide. But when it hides, it hides all videos and shows all, so need to fix that

--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -8,21 +8,27 @@ import {Video} from './video';
 })
 
 export class PlaylistComponent{
+    
+    private hideElement: boolean = true;
+    toggleElement(){
+        console.log("insidethe toggle")
+        if(this.hideElement){
+            console.log("hideElement is true")
+            this.hideElement = false;
+
+        } else{
+            console.log("hideElement is false")
+            this.hideElement = true;
+        }
+    }
     //displays all the videos  
     onSelect(vid:Video){
         // console.log(JSON.stringify(vid.videoCode));
         var vidId = JSON.stringify(vid.id);
-        var show;
-        if (vid.count == 0){
-            show = true;
-            console.log(show);
-            vid.count ++
-        } else {
-            show = false;
-            vid.count = 0;
-            console.log(show);
-        }
+        // this.toggleElement()
+        console.log("insid onSelect")
+        this.toggleElement()
         // document.getElementById("myVid").innerHTML = '<iframe width="420" height="315" [src]="' + "'https://www.youtube.com/embed/'" + vidCode + '"></iframe>'
-        document.getElementById(vidId).innerHTML ='<iframe width="420" height="315" src="'+'https://www.youtube.com/embed/' + vid.videoCode + '"></iframe>';
+        document.getElementById(vidId).innerHTML ='<td><iframe width="420" height="315" src="'+'https://www.youtube.com/embed/' + vid.videoCode + '"></iframe></td>';
     }
 }
--- app/ts/video.ts
+++ app/ts/video.ts
@@ -4,13 +4,11 @@ export class Video {
     title:string;
     videoCode:string;
     desc:string;
-    count:number;
 
-    constructor(id:number,title:string, videoCode:string, desc:string, count:number){
+    constructor(id:number,title:string, videoCode:string, desc:string){
         this.id=id;
         this.title=title;
         this.videoCode=videoCode;
         this.desc=desc;
-        this.count=count;
     }
 }
